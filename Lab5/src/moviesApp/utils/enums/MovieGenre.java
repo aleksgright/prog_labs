8100da1f3c4eff3d5c2f17ce44efa5e27d176f83
aleks_gright <Alexander2326@yandex.ru>
2022-05-19 15:50:49 +0300

    Refactoring repositories

--- Lab5/src/moviesApp/utils/enums/MovieGenre.java
+++
@@ -1,9 +0,0 @@
-package moviesApp.utils.enums;
-
-import java.io.Serializable;
-
-public enum MovieGenre implements Serializable {
-    THRILLER,
-    HORROR,
-    FANTASY
-}
--- Lab5/src/moviesApp/utils/exceptions/MoviesAppException.java
+++
@@ -1,7 +0,0 @@
-package moviesApp.utils.exceptions;
-
-public class MoviesAppException extends RuntimeException {
-    public MoviesAppException(String message) {
-        super(message);
-    }
-}
